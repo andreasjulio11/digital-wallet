2d33ee462424ff7028983903bf715c5d4632df06
andreasjulio11 <andreasjulio94@gmail.com>
2026-02-03 11:18:36 +0700

    -

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -10,7 +10,6 @@ import (
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 1. Ambil header Authorization
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Butuh login (Token tidak ditemukan)"})
@@ -18,12 +17,8 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		// 2. Ambil tokennya saja
 		tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
 
-		// 3. Validasi tokennya
-		// Karena fungsi config.ValidateToken kita sudah mengembalikan Claims,
-		// kita langsung tampung hasilnya di variabel 'claims'
 		claims, err := config.ValidateToken(tokenString)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token tidak valid: " + err.Error()})
@@ -31,8 +26,6 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		// 4. Simpan data user ke dalam Context
-		// Sekarang 'claims' sudah bisa langsung diakses
 		c.Set("user_id", claims["user_id"])
 
 		c.Next()
